817be7f5ef16b55842015285d229cfebb03055a9
adnan-jahbli <adnan.jahbli@gmail.com>
2023-08-29 18:20:46 +0000

    fixing compiling error of file 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,11 +9,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree == NULL)
+	size_t left_height, right_height;
+
+	if (!tree)
 		return (0);
 
-	size_t left_height = binary_tree_height(tree->left);
-	size_t right_height = binary_tree_height(tree->right);
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
 	return (1 + (left_height > right_height ? left_height : right_height));
 }
