ece81ba8b300b48c0e00b7f6cd69555e6ceef087
adnan-jahbli <adnan.jahbli@gmail.com>
2023-08-30 13:16:55 +0000

    creating file 10

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+
+/**
+ * sub_func - a function that measures
+ * the depth of a node in a binary tree
+ * @tree: the binary tree's root
+ * Return: the depth of a node in a binary tree
+ */
+size_t sub_func(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (-1);
+
+	return (sub_func(tree->parent) + 1);
+}
+
+/**
+ * binary_tree_depth - calls sub_func to calculate
+ * and return the depth of a node in a binary tree.
+ *
+ * @tree: Pointer to the root node of the tree.
+ *
+ * Return: Depth of the tree, or 0 if tree is NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (sub_func(tree));
+}
